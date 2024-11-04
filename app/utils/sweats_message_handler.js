c938b537a9ed5eea6aa42db4982ca9b8d28c595f
Kate Kaufmann <emil.kaufmann@stud.hn.de>
2024-11-04 14:40:12 +0100

    added sweats message handler

---
+++ app/utils/sweats_message_handler.js
@@ -0,0 +1,90 @@
+import { DiscordRequest } from './discord_request.js';
+import { EventEmitter } from 'events';
+
+
+export async function sendMessage() {
+    const channelId = process.env.CHANNEL_ID_CSWEATS;
+    const messageId = await sendMessage(channelId);
+    listenForReactions(channelId, messageId);
+}
+
+
+function listenForReactions(channelId, messageId) {
+    const endpoint = `channels/${channelId}/messages/${messageId}/reactions/%F0%9F%92%A7`;
+    const listener = new EventEmitter();
+    let previousUsers = [];
+
+    setInterval(async () => {
+        const response = await DiscordRequest(endpoint, {
+            method: 'GET',
+        });
+        const users = await response.json();
+        const userIds = users.map(user => user.id);
+
+        if (JSON.stringify(userIds) !== JSON.stringify(previousUsers)) {
+            previousUsers = userIds;
+            listener.emit('reaction', users);
+        }
+    }, 5000); // Check every 5 seconds
+
+    listener.on('reaction', async (users) => {
+        users = users.filter(user => !user.bot);
+        if(users.length != 0) {
+            removeOwnReaction(channelId, messageId);
+        }else{
+            addOwnReaction(channelId, messageId);
+        }
+        editMessage(channelId, messageId, users);
+    });
+
+}
+
+
+export async function sendMessage(channelId) {
+    const endpoint = `channels/${channelId}/messages`;
+    const response = await DiscordRequest(endpoint, {
+        method: 'POST',
+        body: {
+            embeds: [{
+                title: 'Sweats Today!',
+                description: 'React with 💧 to sign up for sweats today!\nCurrent signups: None',
+                color: 0x00ff00,
+            }],
+        }
+    });
+    const message= await response.json();
+    addOwnReaction(channelId, message.id);
+    return message.id;
+}
+
+export async function editMessage(channelId, messageId, users) {
+    const endpoint = `channels/${channelId}/messages/${messageId}`;
+    const formattedUserList = users.map(user => `<@${user.id}>`).join('\n');
+    await DiscordRequest(endpoint, {
+        method: 'PATCH',
+        body: {
+            embeds: [{
+                title: 'Sweats Today!',
+                description: 'React with 💧 to sign up for sweats today! \nCurrent signups: \n' + formattedUserList,
+                color: 0x00ff00,
+            }],
+            allowed_mentions: {
+                "parse": ["users"],
+               },
+        }
+    });
+}
+
+export async function removeOwnReaction(channelId, messageId) {
+    const endpoint = `channels/${channelId}/messages/${messageId}/reactions/%F0%9F%92%A7/@me`;
+    await DiscordRequest(endpoint, {
+        method: 'DELETE',
+    });
+}
+
+export async function addOwnReaction(channelId, messageId) {
+    const endpoint = `channels/${channelId}/messages/${messageId}/reactions/%F0%9F%92%A7/@me`;
+    await DiscordRequest(endpoint, {
+        method: 'PUT',
+    });
+}
