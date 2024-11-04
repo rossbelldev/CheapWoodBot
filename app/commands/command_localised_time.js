4269a87d3d4632be423b9175dca0b38bd8380245
rossbelldev <rosscameronclarkbell@gmail.com>
2024-11-04 13:08:58 +0000

    added error handling to date time checker

--- app/commands/command_localised_time.js
+++ app/commands/command_localised_time.js
@@ -1,9 +1,35 @@
 export function getLocalisedTime(date, time, timezone) {
-    return `<t:${getUnixTimeInSeconds(date, time, timezone)}:F>`;
+    let formattedDateTime;
+    try {
+        formattedDateTime = formatDateTime(date, time, timezone);
+    } catch (error) {
+        return error;
+    }
+
+    return `<t:${getUnixTimeInSeconds(formattedDateTime)}:F>`;
 }
 
-function getUnixTimeInSeconds(date, time, timezone) { 
-    const dateTime = `${date}T${time}${timezone}`;
-    const outputDate = new Date(dateTime);
-    return Math.floor(outputDate.getTime() / 1000);
+function formatDateTime(date, time, timezone) {
+    validateDateTimeFormat(date, time, timezone);
+    return `${date}T${time}${timezone}`;
+}
+
+function validateDateTimeFormat(date, time, timezone) {
+    const dateFormatRegex = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+    const timeFormatRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+    const timezoneFormatRegex = /^[+-]((0[0-9]|1[0-1]):[0-5][0-9])|12:00$/;
+
+    // if date, time and timezone are not in the right format
+    if (!dateFormatRegex.test(date)) {
+        throw "Date in incorrect format";
+    } else if (!timeFormatRegex.test(time)) {
+        throw "Time in incorrect format";
+    } else if (!timezoneFormatRegex.test(timezone)) {
+        throw "Timezone in incorrect format";
+    }
+}
+
+function getUnixTimeInSeconds(formattedDateTime) { 
+    const date = new Date(formattedDateTime);
+    return Math.floor(date.getTime() / 1000);
 }
